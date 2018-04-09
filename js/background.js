26599039339771ec74ab7eed24e8c4396ee07843
usmanasif <pyroxicated@gmail.com>
2018-04-09 16:10:41 +0500

    match candidates, call api, refresh candidates

--- js/background.js
+++ js/background.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
   console.log("alarm started");
-  chrome.alarms.create("myAlarm", {delayInMinutes: 1, periodInMinutes: 1} );
+  chrome.alarms.create("candidatesAlarm", {delayInMinutes: 1, periodInMinutes: 1} );
 
   chrome.alarms.onAlarm.addListener(function(alarm) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+      chrome.tabs.sendMessage(tabs[0].id, {greeting: "ready"}, function(response) {
         console.log(response);
         if (response){
-          if(response.farewell == "goodbye"){
+          if(response.farewell == "done"){
             console.log("alarm closed");
-            chrome.alarms.clear("myAlarm");
+            chrome.alarms.clear("candidatesAlarm");
           }
         }
     });
--- js/popup.js
+++ js/popup.js
@@ -1,51 +1,82 @@
-var name_count = 0;
-var names = [];
-var length = 0;
-var prev_length = 0;
-var ids = [];
+var baseUrl = "http://localhost:3000/v1/angels/match_candidates";
+var candidatesArray = [];
+var refreshIntervalId;
 
-$(window).on('load', function(){
-
-  setTimeout(function(){
-    $(".cards > .card").each(function(index){
-      ids[index] = $(this).attr("data-candidate_id");
-    });
-
-    $(".card-content > .details > .title").each(function(index){
-      names[index] = $(this).text();
-      name_count = index;
-    });
-  }, 5000);
-
-});
-
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.greeting == "hello"){
-      console.log("message incoming...");
-      prev_length = names.length;
-      $(".cards > .card").each(function(index){
-        if ($(this).attr("data-candidate_id"))
-          ids[index] = $(this).attr("data-candidate_id");
-      });
-      $(".card-content > .details > .title").each(function(index){
-        if(index >= name_count){
-          names[index] = $(this).text();
-          name_count = index;
-        }
-      });
-      console.log(ids);
-      console.log(names);
-      length = names.length;
-
-      if(length == prev_length) {
-        $(".cards > .card").each(function(index){
-          if ($(this).attr("data-candidate_id")){
-            $(this).append("<div class='u-fontSize4 u-colorGray6 s-vgBorderBottom0_5 s-vgPadTop0_5 s-vgPadBottom0_5 s-vgPadLeft1 s-vgPadRight1'><img style='width:15%;' src="+"'"+chrome.extension.getURL("images/qualified-icon.jpg")+"'"+"> YES YES</div>");
-          }
-        });
-        sendResponse({farewell: "goodbye"});
-
+function onInit(){
+  $(window).on('load', function(){
+    refreshIntervalId = setInterval(function(){
+      if (candidatesArray.length != 0) {
+        clearInterval(refreshIntervalId);
+        console.log(candidatesArray);
+        matchCandidates();
+      } else {
+        candidatesArray = extractCandidates();
       }
-    }
-});
+    }, 2000);
+  });
+}
+
+// extarct candidates data from page
+function extractCandidates(){
+  var candidates = [];
+  $(".cards .card").each(function(index){
+    var candidatesHash = {
+      "angel_id": "",
+      "name": ""
+    };
+    candidatesHash["angel_id"] = $(this).attr("data-candidate_id");
+    candidatesHash["name"] = $(this).find(".card-content .title").text().replace(/[^a-zA-Z ]/g, "");
+    if(candidatesHash['angel_id'] != undefined)
+      candidates.push(candidatesHash);
+  });
+  return candidates;
+}
+
+// update candidates results
+function updateCandidates(data) {
+  $.each( data, function( index, obj ) {
+    console.log(obj);
+    $.each( obj["assessments"], function( i, ob ) {
+      var score = ob["score"] == null ? "0" : ob["score"];
+      $("[data-candidate_id="+ obj['angel_id'] +"]").append('<div style="width: 100%; display: table; width: 100%;"> \
+        <div style="display: inline-block; width: 54px; vertical-align: middle;"> \
+          <img style="width: 46px;" src="'+chrome.extension.getURL("images/qualified-icon.jpg")+'"> \
+        </div> \
+        <div style=" display: inline-block; vertical-align: middle; color: #00a466; font-size: 15px;"> \
+          <i class="fa fa-check-circle" aria-hidden="true"></i> '+ titleize(obj['state']) +' \
+        </div> \
+        <div style="border-radius: 25px; border: 2px solid #00a466; padding: 5px; display: inline-block; vertical-align: middle; float: right; margin: 9px 8px 0 0;">'+ score +'%</div>'
+        );
+    });
+  });
+}
+
+function titleize(sentence) {
+  if(!sentence.split) return sentence;
+  var _titleizeWord = function(string) {
+          return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+      },
+      result = [];
+  sentence.split(" ").forEach(function(w) {
+      result.push(_titleizeWord(w));
+  });
+  return result.join(" ");
+}
+
+// send request to api and match candidates
+function matchCandidates() {
+  $.post( baseUrl,
+    {
+      candidates: JSON.stringify(candidatesArray)
+    },
+    function(data, status){
+      if (data.length != 0) {
+        updateCandidates(data);
+      } else {
+        console.log("No data found!");
+      }
+  });
+}
+
+// start extention
+onInit();
